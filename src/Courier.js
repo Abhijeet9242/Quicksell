7da6af9c72de02b2a1d8047a7eeebdfe03748c5a
Abhijeet9242 <87429783+Abhijeet9242@users.noreply.github.com>
2022-04-27 16:31:24 +0530

    taskcompleted

---
+++ src/Courier.js
@@ -0,0 +1,110 @@
+import "./Courier.css";
+import React from "react";
+import { useEffect, useState } from "react";
+
+const Courier = () => {
+  const [weight, setWeight] = useState("");
+  const [pin, setPincode] = useState("");
+  const [rate, setRate] = useState("");
+  const [charge, setCharge] = useState(0);
+
+  const [companyx, setCompanyx] = useState([]);
+  const [companyy, setCompanyy] = useState([]);
+
+  useEffect(() => {
+    fetch("  http://localhost:3008/companyx")
+      .then((res) => res.json())
+      .then((result) => setCompanyx(result));
+  }, []);
+
+  useEffect(() => {
+    fetch(" http://localhost:3008/companyy  ")
+      .then((res) => res.json())
+      .then((result) => setCompanyy(result));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let zo = companyx.filter((e) => {
+      // console.log(e.pincode);
+      return Number(pin) === e.CustomerPincode;
+    });
+    // console.log(zo);
+    checkzone(zo);
+  };
+
+  const checkzone = (zo) => {
+    // console.log(z);
+    let zone = zo.map((e) => {
+      return e.Zone;
+    });
+
+    let uzone = zone.join("");
+
+    let pric = companyy.filter((e) => {
+      return uzone === e.Zone && rate === e.RateType;
+    });
+    // console.log(pric);
+    checkprice(pric);
+  };
+
+  const checkprice = (data) => {
+    let firstkgprice = data.map((e) => {
+      return e.FirstKG;
+    });
+    let additionalkg = data.map((e) => {
+      return e.Every_AdditionalKG;
+    });
+
+    let firstkg = Number(firstkgprice.join(""));
+    let addikg = Number(additionalkg.join(""));
+
+    calculateprice(firstkg, addikg, weight);
+  };
+
+  const calculateprice = (firstkg, addikg, weight) => {
+    let weightkgprice = Math.round(weight);
+
+    let sum = firstkg;
+    for (let i = 1; i < weightkgprice; i++) {
+      sum += addikg;
+    }
+    let totalprice = sum.toFixed(2);
+    setCharge(totalprice);
+  };
+
+  return (
+    <div className="form_div">
+      <div className="form_div">
+        <form action="">
+          <input
+            type="text"
+            placeholder="enter weight in kg"
+            onChange={(e) => setWeight(e.target.value)}
+            name="weight"
+          />
+          <input
+            type="number"
+            placeholder="enter pincode"
+            onChange={(e) => setPincode(e.target.value)}
+            name="pincode"
+          />
+          <select
+            name="ratetype"
+            id=""
+            onChange={(e) => setRate(e.target.value)}
+          >
+            <option value="Forward">Forward</option>
+            <option value="Forward & RTO">Forward&Rto</option>
+          </select>
+          <button onClick={handleSubmit}>submit</button>
+        </form>
+
+        <div style={{ marginLeft: 20 }}>{charge}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Courier;
